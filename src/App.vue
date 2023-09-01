9d7089e80dbe1dae6650913b988ef2308707f0d1
NEBULA1978 <rpascualey@gmail.com>
2023-09-01 09:42:29 +0200

    Añado boton para ver componente App2.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Padre:</h1>
+    <HijoVue :nombre="nombre" :edad="edad" @modificar="cambiarValor" />
+    <HijoVue2 />
+    <button @click="mostrarApp2">Mostrar/ocultar App2</button>
+    <App2 v-if="mostrarComponenteApp2" />
+  </div>
+</template>
+
+<script setup>
+import { provide, ref } from "vue";
+import HijoVue from "./components/Hijo.vue";
+import HijoVue2 from "./components/Hijo2.vue";
+import App2 from "./components/App2.vue";
+
+const cambiarValor = (valor1, valor2) => {
+  nombre.value = valor1;
+  edad.value = valor2;
+};
+const nombre = ref('Jab');
+const edad = ref(18);
+const canal = ref("soyjab");
+provide('miCanal', canal);
+
+const mostrarComponenteApp2 = ref(false);
+
+const mostrarApp2 = () => {
+  mostrarComponenteApp2.value = !mostrarComponenteApp2.value;
+};
+</script>
+
+<style scoped></style>
